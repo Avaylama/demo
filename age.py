423aa6a044b6dcd21ee7acb324bfc780c99159f7
Avay Lama <avaytamang27@gmail.com>
2024-12-21 22:17:25 +0545

    demo2 removed

---
+++ age.py
@@ -0,0 +1,13 @@
+age = int(input("Enter the marks :"))
+
+if age < 13:
+    print("Child")
+elif age <= 13 and age <= 19:
+    print("Teenager")
+elif age > 19:
+    print("Adult")
+else:
+    pass
+
+
+
---
+++ color.py
@@ -0,0 +1,9 @@
+color = input("Enter the color name : ").lower()
+if color == "red":
+    print("Stop") 
+elif color == "yellow":
+    print("Get Ready")
+elif color == "green":
+    print("Go")
+else:
+    print("Invalid color name")
